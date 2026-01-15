d93f0091123b55641214d0c82771e8c8a9312ca4
Fabiopergent <112112448+Fabiopergent@users.noreply.github.com>
2026-01-15 12:38:14 +0000

    5645546

--- auth.js
+++ auth.js
@@ -1,18 +1,21 @@
-const usuarioLogado = localStorage.getItem('usuarioLogado');
+const usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
 
 if (!usuarioLogado) {
     alert('Acesso negado. Faça login.');
     window.location.href = 'index.html';
 }
 
+// Página atual
 const pagina = window.location.pathname;
 
+// Proteção funcionário
 if (pagina.includes('area-funcionario') && usuarioLogado.tipo !== 'funcionario') {
     alert('Acesso permitido apenas para funcionários.');
-window.location.href = 'index.html';
+    window.location.href = 'index.html';
 }
 
-if (pagina.inclides('area-cliente') && usuarioLogado.tipo !== 'cliente') {
+// Proteção cliente
+if (pagina.includes('area-cliente') && usuarioLogado.tipo !== 'cliente') {
     alert('Acesso permitido apenas para clientes.');
     window.location.href = 'index.html';
 }
