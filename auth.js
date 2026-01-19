e108e215ad5771f2416bdcd453d238f6e43991bf
Fabiopergent <112112448+Fabiopergent@users.noreply.github.com>
2026-01-19 12:30:29 +0000

    atualizacao

--- auth.js
+++ auth.js
@@ -19,3 +19,14 @@ if (pagina.includes('area-cliente') && usuarioLogado.tipo !== 'cliente') {
     alert('Acesso permitido apenas para clientes.');
     window.location.href = 'index.html';
 }
+
+//login usuario/
+const usuario =JSON.parse(localStorage.getItem('usuarioLogadpo'));
+
+if (!usuario) {
+    window.location.href = 'index.html';
+}
+
+if (document.body.dataset.tipo === 'funcionario' && usuario.tipo !== 'funcionario') {
+    window.location.href = 'index.html';
+}
--- script.js
+++ script.js
@@ -53,3 +53,67 @@ if (formLoginFuncionario) {
         window.location.href = 'area-funcionario.html';
     });
 }
+
+//mostrar formulario//
+
+function mostrarCadastro() {
+    document.getElementById('cadastroCliente').styledisplay = 'block';
+}
+
+//cadastrar cliente//
+
+function cadastrarCliente() {
+    const cliente = {
+        id: Date.now(),
+        nome: document.getElementById('nome').value,
+        email: document.getElementById('email').value,
+        senha: document.getElementById('senha').value,
+        cpf: document.getElementById('cpf').value,
+        telefone: document.getElementById('telefone').value,
+        endereco: document.getElementById('endereco').value,
+        consulta: []
+    };
+
+    let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
+
+    //validar email duplicado//
+    if (clientes.some(C => C.email === cliente.email)) {
+        alert('Email já cadastrado');
+        return;
+    }
+
+    clientes.push(cliente);
+    localStorage.setItem('clientes', JSON.stringify(clientes));
+
+    alert('CLiente cadastrado com sucesso!');
+}
+
+//exibir dados do cliente//
+// Só roda se estiver na área do cliente
+if (window.location.pathname.includes('area-cliente')) {
+
+    const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
+
+    if (usuario && usuario.tipo === 'cliente') {
+
+        const dadosDiv = document.getElementById('dados');
+        const lista = document.getElementById('consultas');
+
+        dadosDiv.innerHTML = `
+            <p><b>Nome:</b> ${usuario.nome}</p>
+            <p><b>Email:</b> ${usuario.email}</p>
+            <p><b>CPF:</b> ${usuario.cpf}</p>
+            <p><b>Telefone:</b> ${usuario.telefone}</p>
+            <p><b>Endereço:</b> ${usuario.endereco}</p>
+        `;
+
+        // Evita erro se ainda não tiver consultas
+        if (usuario.consultas && usuario.consultas.length > 0) {
+            usuario.consultas.forEach(c => {
+                lista.innerHTML += `<li>${c.data} - ${c.horario} (${c.tipo})</li>`;
+            });
+        } else {
+            lista.innerHTML = '<li>Nenhuma consulta agendada</li>';
+        }
+    }
+}
